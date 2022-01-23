7bb27e2060934672e8245fa3492f344badcd1081
mokoshin0720 <shinya.work0720@gmail.com>
2022-01-23 22:46:02 +0900

    [add]get errors

--- go/pkg/empath/empath.go
+++ go/pkg/empath/empath.go
@@ -1,35 +1,40 @@
 package empath
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
-	"net/url"
 	"os"
-	"strings"
 
 	"github.com/joho/godotenv"
 )
 
 type (
 	Result struct {
-		Message string
+		Message  string
+		Response Response
 	}
 
 	Response struct {
-		Error  string `json:"error"`
-		Calm   string `json:"calm"`
-		Anger  string `json:"anger"`
-		Joy    string `json:"joy"`
-		Sorrow string `json:"sorrow"`
-		Energy string `json:"energy"`
+		Error  int64 `json:"error"`
+		Calm   int64 `json:"calm"`
+		Anger  int64 `json:"anger"`
+		Joy    int64 `json:"joy"`
+		Sorrow int64 `json:"sorrow"`
+		Energy int64 `json:"energy"`
+	}
+
+	Request struct {
+		APIKey string         `json:"apikey"`
+		Wav    multipart.File `json:"wav"`
 	}
 )
 
 const (
-	URI string = "'https://api.webempath.net/v2/analyzeWav'"
+	URI string = "https://api.webempath.net/v2/analyzeWav"
 )
 
 func Analyze(file multipart.File) (*Result, error) {
@@ -39,44 +44,38 @@ func Analyze(file multipart.File) (*Result, error) {
 	}
 	apiKey := os.Getenv("EMPATH_API_KEY")
 
-	f, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-
-	r, err := empath_api(f, apiKey)
+	r, err := empath_api(file, apiKey)
 	if err != nil {
 		return nil, err
 	}
 	fmt.Println(r)
 
-	return &Result{Message: "empath"}, nil
+	return &Result{Message: "empath", Response: *r}, nil
 }
 
-func empath_api(file []byte, apiKey string) (response *Response, err error) {
-	values := url.Values{}
-	values.Set("apikey", apiKey)
-	values.Set("wav", string(file))
-
-	req, err := http.NewRequest("POST", URI, strings.NewReader(values.Encode()))
+func empath_api(file multipart.File, apiKey string) (response *Response, err error) {
+	req := Request{APIKey: apiKey, Wav: file}
+	j, err := json.Marshal(req)
 	if err != nil {
+		fmt.Println("here1")
 		return nil, err
 	}
 
-	client := new(http.Client)
-	resp, err := client.Do(req)
+	res, err := http.Post(URI, "application/json", bytes.NewBuffer(j))
 	if err != nil {
+		fmt.Println("here2")
 		return nil, err
 	}
-	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
+		fmt.Println("here3")
 		return nil, err
 	}
 
 	var r Response
 	if err := json.Unmarshal(body, &r); err != nil {
+		fmt.Println("here3")
 		return nil, err
 	}
 
